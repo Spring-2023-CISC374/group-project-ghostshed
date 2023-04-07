3c117a77e1232dcc77d7105d03c0a7e44da9cc16
BenRaymon <bendraymon@gmail.com>
2023-04-07 14:57:14 -0400

    created retreat function to reverse nagivate the ghost back to the spawn point

--- src/scripts/objects/ghost.ts
+++ src/scripts/objects/ghost.ts
@@ -3,7 +3,8 @@ export default class Ghost extends Phaser.Physics.Arcade.Sprite {
     // https://gamedevacademy.org/how-to-make-tower-defense-game-with-phaser-3/
     follower: { t: number, vec: Phaser.Math.Vector2};
     zonePath?: Phaser.Curves.Path;
-    GHOST_SPEED: number = 1/8000;
+    GHOST_SPEED: number = 1/5000;
+    isInPlayerZone: boolean = false;
 
     constructor(scene: Phaser.Scene, zone: number) {
         super(scene, 0, 0, 'dude');
@@ -11,27 +12,27 @@ export default class Ghost extends Phaser.Physics.Arcade.Sprite {
 
         this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
 
-        let graphics = scene.add.graphics();    
-        graphics.lineStyle(3, 0xffffff, 1);
+        //let graphics = scene.add.graphics();    
+        //graphics.lineStyle(3, 0xffffff, 1);
 
         // Draw the right path for the corresponding zone
         if (zone === 2) {
             this.zonePath = scene.add.path(130, 100);
             this.zonePath.lineTo(80, 100);
             this.zonePath.lineTo(80, 350);
-            this.zonePath.draw(graphics);
-        } else if (zone === 3) {
+            //this.zonePath.draw(graphics);
+        } else if (zone === 4) {
             this.zonePath = scene.add.path(280, 100);
             this.zonePath.lineTo(360, 100);
-            this.zonePath.draw(graphics);
+            //this.zonePath.draw(graphics);
 
             // zone 3 ghost moves back and forth faster
             this.GHOST_SPEED *= 2;
-        } else if (zone === 4) {
+        } else if (zone === 3) {
             this.zonePath = scene.add.path(510, 100);
             this.zonePath.lineTo(560, 100);
             this.zonePath.lineTo(560, 350);
-            this.zonePath.draw(graphics);
+            //this.zonePath.draw(graphics);
         }
     }
 
@@ -47,14 +48,32 @@ export default class Ghost extends Phaser.Physics.Arcade.Sprite {
 
     update(time: any, delta: any)
     {
-        // move the t point along the path, 0 is the start and 0 is the end
-        this.follower.t += this.GHOST_SPEED * delta;
+        if (this.follower.t <= 1 && this.follower.t >= 0){
+            // move the t point along the path, 0 is the start and 0 is the end
+            this.follower.t += this.GHOST_SPEED * delta;
+
+            // get the new x and y coordinates in vec
+            this.zonePath?.getPoint(this.follower.t, this.follower.vec);
+
+            // update enemy x and y to the newly obtained x and y
+            this.setPosition(this.follower.vec.x, this.follower.vec.y);
+        } else if (this.follower.t > 1) {
+            this.follower.t = 1;
+        } else if (this.follower.t < 0){
+            this.follower.t = 0;
+        }
         
-        // get the new x and y coordinates in vec
-        this.zonePath?.getPoint(this.follower.t, this.follower.vec);
-        
-        // update enemy x and y to the newly obtained x and y
-        this.setPosition(this.follower.vec.x, this.follower.vec.y);
+        this.isInPlayerZone = this.follower.t >= 1;
+    }
+
+    retreat(){
+        if (this.isInPlayerZone){
+            console.log("IN ZONE");
+            this.GHOST_SPEED = this.GHOST_SPEED * -1;
+            // initiate the reverse
+            this.zonePath?.getPoint(this.follower.t, this.follower.vec);
+            this.setPosition(this.follower.vec.x, this.follower.vec.y);
+        }
     }
 
 
