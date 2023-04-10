af2e64a6579c4e0036ea3ced25a2b72672ebf43f
BenRaymon <bendraymon@gmail.com>
2023-04-10 14:34:19 -0400

    testing ghost functions in roomscene

--- src/scripts/scenes/RoomScene.ts
+++ src/scripts/scenes/RoomScene.ts
@@ -1,5 +1,6 @@
 import Phaser from 'phaser'
 import  { findPath } from '../objects/findPath';
+import Ghost from '../objects/ghost';
 
 export default class RoomScene extends Phaser.Scene {
 
@@ -7,7 +8,9 @@ export default class RoomScene extends Phaser.Scene {
 	private tiles!: Phaser.Tilemaps.Tileset 
 	private player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
 	private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
-	private curZone = "Zone 1"
+	private curZone: number = 1
+
+	private ghosts: Ghost[] = []
 
 	//movement variables
 	private movePath?: Phaser.Math.Vector2[]
@@ -19,18 +22,23 @@ export default class RoomScene extends Phaser.Scene {
 
 	preload() {
 		this.load.image('tileset_image', 'assets/tilemaps/tileset.png')
-    this.load.tilemapTiledJSON('tilemap', 'assets/tilemaps/main.json')
+    	this.load.tilemapTiledJSON('tilemap', 'assets/tilemaps/main.json')
 		this.load.image('player', 'assets/tilemaps/character.png')
+
+		this.load.spritesheet('dude',
+			'/assets/dude_edited.png',
+			{ frameWidth: 32, frameHeight: 48 }
+		)
 	}
 
 	// this trigger action code
-	updateZone(newZone: string){
+	updateZone(newZone: number){
 		this.curZone = newZone
 	}
 	
 	hide(){
 		console.log("Trying to Hide")
-		if(this.curZone == "Zone 4"){
+		if(this.curZone == 4){
 			console.log("Hiding")
 		}else{
 			console.log("No wheres to hide")
@@ -40,8 +48,13 @@ export default class RoomScene extends Phaser.Scene {
 	create() {
   		this.map = this.make.tilemap({ key: 'tilemap', tileHeight: 32, tileWidth: 32 })
     	this.tiles = this.map.addTilesetImage('tileset', 'tileset_image')
+		// the index of the ghost is zone # - 2
+		this.ghosts.push(new Ghost(this, 2))
+		//this.ghosts.push(new Ghost(this, 3))
+		//this.ghosts.push(new Ghost(this, 4))
 
-    // Render the layers in Phaser
+
+    	// Render the layers in Phaser
     	for (const layerName of this.map.getTileLayerNames()) {
       		this.map.createLayer(layerName, this.tiles, 0, 0)
     	}
@@ -51,32 +64,37 @@ export default class RoomScene extends Phaser.Scene {
 
 		this.cursors = this.input.keyboard.createCursorKeys();
 
-		this.map.setTileIndexCallback(435, () => { this.updateZone("Zone 2")}, this, "Zone 2");
+		this.map.setTileIndexCallback(435, () => { this.updateZone(2)}, this, "Zone 2");
 		const zone2 = this.map.getLayer("Zone 2").tilemapLayer
 		this.physics.add.overlap(this.player, zone2);
 
-		this.map.setTileIndexCallback(486, () => { this.updateZone("Zone 1")}, this, "Zone 1");
+		this.map.setTileIndexCallback(486, () => { this.updateZone(1)}, this, "Zone 1");
 		const zone1 = this.map.getLayer("Zone 1").tilemapLayer
 		this.physics.add.overlap(this.player, zone1);
 
-		this.map.setTileIndexCallback(436, () => { this.updateZone("Zone 3")}, this, "Zone 3");
+		this.map.setTileIndexCallback(436, () => { this.updateZone(3)}, this, "Zone 3");
 		const zone3 = this.map.getLayer("Zone 3").tilemapLayer
 		this.physics.add.overlap(this.player, zone3);
 
-		this.map.setTileIndexCallback(434, () => { this.updateZone("Zone 4")}, this, "Zone 4");
+		this.map.setTileIndexCallback(434, () => { this.updateZone(4)}, this, "Zone 4");
 		const zone4 = this.map.getLayer("Zone 4").tilemapLayer
 		this.physics.add.overlap(this.player, zone4);
 
-		this.map.setTileIndexCallback(338, () => { this.updateZone("Zone 0")}, this, "Ground");
+		this.map.setTileIndexCallback(338, () => { this.updateZone(0)}, this, "Ground");
 		const zone0 = this.map.getLayer("Ground").tilemapLayer
 		this.physics.add.overlap(this.player, zone0);
+
+		const TESTKEY = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+		TESTKEY.on('down',  (key:any, event:any) => {
+			this.ghosts[0].startOnPath();
+		});
 
 		const hKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.H);
 
 		hKey.on('down',  (key:any, event:any) => {
 
 			console.log("Trying to Hide")
-			if(this.curZone == "Zone 4"){
+			if(this.curZone == 4){
 				console.log("Hiding")
 			}else{
 				console.log("No wheres to hide")
@@ -89,8 +107,9 @@ export default class RoomScene extends Phaser.Scene {
 		fKey.on('down',  (key:any, event:any) => {
 
 			console.log("Using Flashlight")
-			if(this.curZone == "Zone 2" || this.curZone == "Zone 3"){
+			if(this.curZone == 2 || this.curZone == 3){
 				console.log("Shining Bright")
+				this.ghosts[this.curZone - 2].retreat();
 			}else{
 				console.log("Wasting Light")
 			}
@@ -102,8 +121,9 @@ export default class RoomScene extends Phaser.Scene {
 		dKey.on('down',  (key:any, event:any) => {
 
 			console.log("Trying to close door")
-			if(this.curZone == "Zone 2" || this.curZone == "Zone 3"){
+			if(this.curZone == 2 || this.curZone == 3){
 				console.log("Door Closed")
+				this.ghosts[this.curZone - 2].retreat();
 			}else{
 				console.log("No door close")
 			}
@@ -115,7 +135,7 @@ export default class RoomScene extends Phaser.Scene {
 		cKey.on('down',  (key:any, event:any) => {
 
 			console.log("Trying to Blow out candle")
-			if(this.curZone == "Zone 1"){
+			if(this.curZone == 1){
 				console.log("Summoning Delayed")
 			}else{
 				console.log("Wasting Breath")
@@ -187,7 +207,14 @@ export default class RoomScene extends Phaser.Scene {
 
     }
 
-	update() {
+	update(time: any, delta: any) {
+		for (let ghost of this.ghosts){
+			let ghostsWin = ghost.update(time, delta);
+			if (ghostsWin){
+				console.log("THE GAME IS OVER. THE GHOSTS WIN");
+			}
+		}
+
 		let dx = 0
 		let dy = 0
 		if (this.endPath){
