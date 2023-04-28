65c963582ac15b84f4055d4fdb09dabbc55f7cdd
BenRaymon <bendraymon@gmail.com>
2023-04-28 13:30:47 -0400

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-	plugins: [],
-	server: { host: '0.0.0.0', port: 8000 },
-	clearScreen: false,
-	base: "/group-project-ghostshed/"
-})
