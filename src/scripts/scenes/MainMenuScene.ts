928de0b77645341a00a8320eea25d722447249af
Aaron Liu <olympus@udel.edu>
2023-05-03 13:52:26 -0400

    removed old assess for previous background and used the center variables for the background

--- src/scripts/scenes/MainMenuScene.ts
+++ src/scripts/scenes/MainMenuScene.ts
@@ -3,8 +3,6 @@ import Button from '../objects/Button'
 
 export default class MainMenuScene extends Phaser.Scene {
 
-  trees!: Phaser.GameObjects.TileSprite
-
 	constructor () {
 		super({ key: 'MainMenu' })
 	}
@@ -14,26 +12,17 @@ export default class MainMenuScene extends Phaser.Scene {
     const { width, height } = this.scale // width and height of the current view
     const centerX = width / 2
     const centerY = height / 2
-
-    //this.trees = this.add.tileSprite(0, 0, 0, 0, 'background').setOrigin(0, 0);
     
-    this.add.video(400, 250, "Start-Animation").play(true)
+    this.add.video(centerX, centerY, "Start-Animation").play(true)
 
-    
-    //this.trees.setScale(1, 0.75)
+    new Button(centerX - 75, centerY - 25, 'Play', this, () => { this.handlePlay() })
+    new Button(centerX- 75, centerY - 100, 'Tutorial', this, () => { this.handleTutorial() })
 
-    new Button(200, 225, 'Play', this, () => { this.handlePlay() })
-    new Button(325, 225, 'Tutorial', this, () => { this.handleTutorial() })
-
-    this.add.text(300, 50, 'Ghost Shed')
+    this.add.text(centerX-100, centerY/2, 'Ghost Shed')
     .setFontSize(60)
     .setOrigin(0.5, 0.5)
     .setColor("Red")
 
-  }
-
-  update () {
-    //this.trees.tilePositionX -= 1;
   }
 
   handlePlay () {
