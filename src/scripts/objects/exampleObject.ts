4a8a5b09209c23dc171a808cbae87df46c81b596
Jaydon Reap <jaydonreap@gmail.com>
2023-05-10 13:49:17 -0400

    Patch

--- src/scripts/objects/exampleObject.ts
+++
@@ -1,7 +0,0 @@
-export default class ExampleObject extends Phaser.GameObjects.Sprite {
-
-    constructor(scene: Phaser.Scene, x: number, y: number) {
-        super(scene, x, y, 'example-object');
-        scene.add.existing(this);
-    }
-}
